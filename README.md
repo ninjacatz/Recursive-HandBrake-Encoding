48283407938c3dc19c1e9afe4aed19bf7fbc910b
ninjacatz <ninjacats5150@gmail.com>
2022-01-11 16:49:12 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+A simple Python script which recursively searches through a given directory and encodes any videos (with given file extensions) via HandBrakeCLI, replacing old videos and keeping the original file extension.
+
+Using this script can greatly reduce the disk space of a collection of uncompressed or lightly compressed videos in one quick step.
+
+Due to the irreversible nature of this script it is HIGHLY recommended that you backup any directories you plan to use OR that you alter to script so that the old, unencoded videos are not deleted.
+
+Download HandBrakeCLI here:
+https://handbrake.fr/downloads2.php
---
+++ script.py
@@ -0,0 +1,48 @@
+import subprocess
+import os
+
+# ------User-Defined Variables-------
+# Directory to begin process:
+start_directory = r''
+# Directory of HandBrakeCLI executable:
+handbrakeCLI_executable_path = r''
+# File extensions to encode:
+video_extensions_to_encode = ('.mov', '.avi', '.mp4', '.m4v')
+# Handbrake Command:
+# Add any additional customization to the end of the list
+# See https://handbrake.fr/docs/en/latest/cli/cli-options.html for more information
+handbrake_command = [
+    handbrakeCLI_executable_path,
+    '-i',
+    'source file path(will be known, do not change)',
+    '-o',
+    'destination path(will be known, do not change)'
+]
+
+def recursiveSearch(directory):
+    for file in os.listdir(directory):
+        file_path = os.path.join(directory, file)
+        if file.endswith(video_extensions_to_encode):
+            performHandbrakeEncoding(directory, file_path, file)
+        elif os.path.isdir(file_path):
+            recursiveSearch(file_path)
+
+def performHandbrakeEncoding(directory, video_path, video):
+    os.chdir(directory)
+    # rename original video
+    os.rename(video, "renamed-" + video)
+    # set renamed video as source file
+    handbrake_command[2] = os.path.join(directory, 'renamed-' + video)
+    # set original video name as destination
+    handbrake_command[4] = video_path
+    # perform encoding with HandBrakeCLI
+    subprocess.run(handbrake_command, shell=True)
+    # remove renamed original video
+    os.remove('renamed-' + video)
+
+recursiveSearch(start_directory)
+
+
+
+
+
